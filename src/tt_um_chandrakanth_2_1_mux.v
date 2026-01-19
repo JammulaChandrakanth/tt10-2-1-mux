390f59b68dfb2620763b647ffdc11fafe2e3a08f
JammulaChandrakanth <jammulachandrakanth@gmail.com>
2026-01-19 18:44:25 -0500

    Update and rename project.v to tt_um_chandrakanth_2_1_mux.v

--- src/tt_um_chandrakanth_2_1_mux.v
+++ src/tt_um_chandrakanth_2_1_mux.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_chandrakanth_2_1_mux (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,28 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    wire A = ui_in[0];
+    wire B = ui_in[1];
+    wire S = ui_in[2];
+    wire O;
+
+    assign O = S ? B : A;
+
+    // Assign outputs
+    assign uo_out[0] = O;
+    assign uo_out[1] = 1'b0;
+    assign uo_out[2] = 1'b0; 
+    assign uo_out[3] = 1'b0; 
+    assign uo_out[4] = 1'b0; 
+    assign uo_out[5] = 1'b0; 
+    assign uo_out[6] = 1'b0; 
+    assign uo_out[7] = 1'b0;
+    
+    // All output pins must be assigned. If not used, assign to 0.
+    assign uio_out = 0;
+    assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, clk, rst_n, ui_in[7:3], uio_in, 1'b0};
 
 endmodule
